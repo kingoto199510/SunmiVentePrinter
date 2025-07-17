126c2ce4067ec48060385929536e32a8676c1588
unknown <vkingoto@gmail.com>
2025-07-17 15:40:59 +0200

    Ajout du projet Android Sunmi avec impression

---
+++ app/src/main/java/com/example/sunmiprinter/MainActivity.java
@@ -0,0 +1,19 @@
+
+package com.example.sunmiprinter;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://nyumbanigrill.com/vente");
+        setContentView(webView);
+    }
+}
